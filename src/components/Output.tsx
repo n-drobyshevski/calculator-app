55cdc026f234ce921f1228bb7722f6388db08c58
Nick <n.drobyshevski@pm.me>
2023-01-31 20:44:39 +0100

    Add Output Component UI

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,4 +1,15 @@
-const Output: React.FC = () => {
-  return <div className="h-2/6 w-full bg-neutral-400"> </div>;
+interface OutputProps {
+  output: string;
+}
+const Output: React.FC<OutputProps> = ({ output }) => {
+  return (
+    <div className="flex h-24 w-full flex-col justify-center rounded-xl border bg-neutral-300 p-4">
+      <input
+        className="w-full appearance-none bg-transparent font-mono focus:outline-none"
+        type="text"
+        value={"output"}
+      />
+    </div>
+  );
 };
 export default Output;
