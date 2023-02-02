f790267f81d9db4ab4f33665988964c07becb59c
Nick <n.drobyshevski@pm.me>
2023-02-02 12:29:28 +0100

    Add: Cursor component UI

---
+++ src/components/Cursor.tsx
@@ -0,0 +1,7 @@
+const Cursor = () => {
+  return (
+    <div className="animation_blink h-[1rem] w-[2px] bg-neutral-900"></div>
+  );
+};
+
+export default Cursor;
--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
-
+import Cursor from "./Cursor";
 interface OutputProps {
   output: string[];
 }
 const Output: React.FC<OutputProps> = ({ output }) => {
   const [strOutput, setStrOutput] = useState<string>("");
+
   useEffect(() => {
     const newOutput: string = output.join("");
     setStrOutput(newOutput);
   }, [output]);
+
   return (
     <div className="flex h-24 w-full flex-col justify-center rounded-xl border bg-neutral-300 p-4 shadow-inner shadow-neutral-400">
-      <p className="w-full select-all bg-transparent font-mono ">{strOutput}</p>
+      <div className="items-center flex justify-start">
+        <p className="select-all bg-transparent font-mono ">{strOutput}</p>
+        <Cursor />
+      </div>
     </div>
   );
 };
